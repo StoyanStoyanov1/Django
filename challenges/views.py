2645fc8d761837147c944a70ec24575774cb5d30
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-03-26 18:44:19 +0100

    tamplates

--- challenges/views.py
+++ challenges/views.py
@@ -1,15 +1,58 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from calendar import c
+from urllib import response
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.template.loader import render_to_string
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Drink at least 2 liters of water daily!",
+    "may": "Read at least 10 pages of a book every day!",
+    "june": "Try a new hobby or skill each week!",
+    "july": "Meditate for at least 10 minutes every day!",
+    "august": "Write down three things you're grateful for every day!",
+    "september": "Limit social media usage to 30 minutes per day!",
+    "october": "Do at least 10 push-ups and 10 sit-ups daily!",
+    "november": "Avoid added sugar for the whole month!",
+    "december": "Perform one random act of kindness every day!"
+}
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month not in range(1, len(months)):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
-
-    challenge_text = {
-        "january": "Eat no meat for the entire month!",
-        "february": "Walk for at least 20 minutes every day!",
-        "march": "Learn Django for at least 20 minutes every day!"
-    }
-
-    if month not in challenge_text:
-        return HttpResponseNotFound("This month is not supoorted!")
-    
-    return HttpResponse(challenge_text[month])
+    try:
+        challenge_text = monthly_challenges[month]
+        
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month.capitalize(),
+        })
+        
+    except:
+        return HttpResponseNotFound("<h1>This month is not supoorted!</h1>")
