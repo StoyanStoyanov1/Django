7ef8786b0989dadd58b64f5b643f06af980b19f9
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-03-30 23:25:53 +0200

    update

--- petstagram/petstagram/common/urls.py
+++ petstagram/petstagram/common/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = ()
+from django.urls import path
+from petstagram.common.views import index
+
+urlpatterns = (
+    path("", index, name="index"),
+)
--- petstagram/petstagram/photos/views.py
+++ petstagram/petstagram/photos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def create_photo(request):
+    context = {}
+    return render(request, "photos/create_photo.html", context)
+
+def details_photo(request, pk):
+    context = {}
+    return render(request, "photos/photo-details-page.html", context)
+
+def edit_photo(request, pk):
+    context = {}
+    return render(request, "photos/photo-edit-page.html", context)
