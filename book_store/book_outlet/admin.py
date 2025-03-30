2987959fa60fee3e6e26c836e50d88c4fe5c52f2
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-03-30 12:34:01 +0200

    Relationships

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author, Address, Country
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -8,4 +8,15 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("author", "rating")
     list_display = ("title", "author")
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("first_name", "last_name")
+    list_display = ("first_name", "last_name")
+
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
+admin.site.register(Country)
