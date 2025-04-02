a2f8e68cb250bfd251d614655075f16cc0176038
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-04-02 13:41:14 +0200

    update

--- my_music_app/my_music_app/settings.py
+++ my_music_app/my_music_app/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'my_music_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
