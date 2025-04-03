5b71a6ce885ff9ba7cca75ba7ce468e9b111e298
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-04-03 10:28:09 +0200

    update

--- my_music_app/my_music_app/web/views.py
+++ my_music_app/my_music_app/web/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
+from django import forms
+from django.shortcuts import redirect
 
 from my_music_app.profiles.models import Profile
+from my_music_app.web.forms import CreateProfileForm
+
+from my_music_app.albums.models import Album
+
 
 def get_profile():
     return Profile.objects.first()
 
 def create_profile(request):
-    return render(request, 'web/home-no-profile.html')
+    form = CreateProfileForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("index")
+        
+    context = {
+        "form": form,
+    }
+
+    return render(request, 'web/home-no-profile.html', context)
 
 
 def index(request):
@@ -14,4 +29,9 @@ def index(request):
 
     if profile is None:
         return create_profile(request)
+
+    context = {
+        "albums", Album.objects.all(),
+
+    }
     return render(request, "web/home-with-profile.html")
