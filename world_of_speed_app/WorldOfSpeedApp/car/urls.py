c4cf3dacf0f867487800b02c16884d5157c31c7e
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-04-04 12:05:23 +0200

    navigate

--- world_of_speed_app/WorldOfSpeedApp/car/urls.py
+++ world_of_speed_app/WorldOfSpeedApp/car/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from WorldOfSpeedApp.car.views import catalogue, car_create, car_edit, car_delete, car_details
 
 urlpatterns = [
-    path('catalogue/', catalogue, name='catalogue'),
-    path('create/', car_create, name='car-create'),
+    path('catalogue/', catalogue, name='catalogue car'),
+    path('create/', car_create, name='create car'),
     path('<int:pk>/', include([
-        path('details/', car_details, name='details'),
-        path('edit', car_edit, name='edit'),
-        path('delete', car_delete, name='delete'),
+        path('details/', car_details, name='details car'),
+        path('edit/', car_edit, name='edit car'),
+        path('delete/', car_delete, name='delete car'),
     ]))
 ]
--- world_of_speed_app/WorldOfSpeedApp/profiles/urls.py
+++ world_of_speed_app/WorldOfSpeedApp/profiles/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from WorldOfSpeedApp.profiles.views import profile_create, profile_delete, profile_edit, profile_details
 
 urlpatterns = [
-    path('create/', profile_create, name='create'),
-    path('details', profile_details, name='details'),
-    path('edit/', profile_edit, name='edit'),
-    path('delete/', profile_delete, name='delete'),
+    path('create/', profile_create, name='create profile'),
+    path('details', profile_details, name='details profile'),
+    path('edit/', profile_edit, name='edit profile'),
+    path('delete/', profile_delete, name='delete profile'),
 ]
