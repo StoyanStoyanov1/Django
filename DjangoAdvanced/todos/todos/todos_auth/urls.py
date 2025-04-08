10e40db0e44c140dbc4a2faaff25284f52dc79ff
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-04-08 22:30:36 +0200

    Django REST

--- DjangoAdvanced/todos/todos/todos_auth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from todos.todos_auth.views import CreateUserView, LoginAppView
-
-urlpatterns = [
-   path("register/", CreateUserView.as_view(), name="api_create_user"),
-   path("login/", LoginAppView.as_view(), name="api_login_user"),
-]
